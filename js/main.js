903a2b221fdd8ee2f86a940b09adac2f3fda8c6b
Harshit Jindal <harshit.jindal@icloud.com>
2020-05-14 02:02:50 +0530

    Fix menu-burger animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 
-let showMenu = False;
+let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
